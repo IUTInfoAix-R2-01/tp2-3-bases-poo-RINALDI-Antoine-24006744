6f4816690502670c36b425a5b51b4342fdda8722
RINALDI-Antoine-24006744 <antoine.rinaldi@etu.univ-amu.fr>
2025-01-28 17:27:44 +0100

    Exo TP

---
+++ src/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+    double radius;
+    public Circle() {
+        radius = 1.2;
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
